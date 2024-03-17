a1ae907a5215084a7418494b2dc6a9f1fe1c7c69
wedho <wellingtond99@gmail.com>
2024-03-17 21:27:57 +0200

    second commit

---
+++ script.js
@@ -0,0 +1,13 @@
+
+// Function to show the modal and center it
+const showModal = () => {
+    const modal = document.getElementById("modalSheet");
+    modal.style.display = "block";
+};
+
+// Function to close the modal and center it
+const closeModal = () => {
+    const modal = document.getElementById("modalSheet");
+    modal.style.display = "none";
+};
+
